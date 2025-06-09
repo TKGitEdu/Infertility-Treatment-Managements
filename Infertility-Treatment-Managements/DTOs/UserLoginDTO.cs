fa6099306c923c4892393371a0b2842f6941fc43
Đặng Quốc Tâm <tamdqse171713@fpt.edu.vn>
2025-06-09 16:34:00 +0700

    sa

---
+++ Infertility-Treatment-Managements/DTOs/UserLoginDTO.cs
@@ -0,0 +1,8 @@
+namespace Infertility_Treatment_Managements.DTOs
+{
+    public class UserLoginDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
