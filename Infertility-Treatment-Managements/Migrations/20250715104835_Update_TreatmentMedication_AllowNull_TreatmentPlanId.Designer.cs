c5b86dc0eded3ade7186df18434df8171e35e8e2
Đặng Quốc Tâm <tamdqse171713@fpt.edu.vn>
2025-07-15 17:49:13 +0700

    dong bo migration vs v1

--- Infertility-Treatment-Managements/Migrations/20250715104835_Update_TreatmentMedication_AllowNull_TreatmentPlanId.Designer.cs
+++ Infertility-Treatment-Managements/Migrations/20250715104835_Update_TreatmentMedication_AllowNull_TreatmentPlanId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infertility_Treatment_Managements.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infertility_Treatment_Managements.Migrations
 {
     [DbContext(typeof(InfertilityTreatmentManagementContext))]
-    partial class InfertilityTreatmentManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20250715104835_Update_TreatmentMedication_AllowNull_TreatmentPlanId")]
+    partial class Update_TreatmentMedication_AllowNull_TreatmentPlanId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
