8044f6add6fee9be22363408e39b71005d6d9171
Đặng Quốc Tâm <tamdqse171713@fpt.edu.vn>
2025-07-15 17:47:11 +0700

    update migration cho phép treatmentplainid null

---
+++ Infertility-Treatment-Managements/Migrations/20250715103846_Update_TreatmentMedication_AllowNull_TreatmentPlanId.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infertility_Treatment_Managements.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_TreatmentMedication_AllowNull_TreatmentPlanId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TreatmentMedications_TreatmentPlans_TreatmentPlanID",
+                table: "TreatmentMedications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TreatmentPlanID",
+                table: "TreatmentMedications",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TreatmentMedications_TreatmentPlans_TreatmentPlanID",
+                table: "TreatmentMedications",
+                column: "TreatmentPlanID",
+                principalTable: "TreatmentPlans",
+                principalColumn: "TreatmentPlanID",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TreatmentMedications_TreatmentPlans_TreatmentPlanID",
+                table: "TreatmentMedications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TreatmentPlanID",
+                table: "TreatmentMedications",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TreatmentMedications_TreatmentPlans_TreatmentPlanID",
+                table: "TreatmentMedications",
+                column: "TreatmentPlanID",
+                principalTable: "TreatmentPlans",
+                principalColumn: "TreatmentPlanID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
